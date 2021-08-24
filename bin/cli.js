5d7b20be5bfccaefe4973b02155b263f1e69a30f
fency <1096316446@qq.com>
2021-08-24 17:24:43 +0800

    feat: 增加获取版本号的功能

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,11 @@
 #! /usr/bin/env node
 
-console.log('Hello fency-cli')
+const { program } = require('commander')
+const package=require('../package.json')
+
+
+//获取package.json的版本号
+program.version(package.version,'-v, --version', '获取当前版本号')
+
+//解析命令行的指令
+program.parse(process.argv)
